758c2a932d9a7b856b2ca451e2d675a51ac015b0
Valeria Chikina <dliavajnogo@gmail.com>
2026-01-21 11:47:43 +0200

    Add class ex 1 21.01.2026

---
+++ class_ex1.py
@@ -0,0 +1,32 @@
+import requests
+
+def download_tampere_rss(url, filename):
+    try:
+        print(f"Загрузка данных с {url}...")
+        response = requests.get(url)
+
+        # Проверяем, успешно ли прошел запрос
+        response.raise_for_status()
+
+        with open(filename, 'w', encoding='utf-8') as file:
+            file.write(response.text)
+
+        print(f"File successfully saved as: {filename}")
+
+        # Вывод атрибуции согласно лицензии CC BY 4.0
+        print("\n--- Attribution ---")
+        print("Source: Latest bulletins from the City of Tampere.")
+        print("Maintainer: Viestintäyksikkö.")
+        print("License: Creative Commons Attribution 4.0")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error while loading: {e}")
+    except IOError as e:
+        print(f"Error while writing file: {e}")
+
+
+rss_url = "https://www.tampere.fi/tampereen-kaupunki/ajankohtaista/tiedotteet/rss.xml.stx"
+output_file = "tampere_bulletins.xml"
+
+if __name__ == "__main__":
+    download_tampere_rss(rss_url, output_file)
